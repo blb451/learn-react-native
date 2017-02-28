73a495e847028d37dc628cb2e815aa8b7d10937e
Benjamin Brenner <ben.l.brenner@gmail.com>
2017-02-28 13:40:59 -0800

    7.37

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
+
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
   state = { albums: [] };
 
   componentWillMount() {
       axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-      .then(response => this.setState({ albums: response.data }));
+        .then(response => this.setState({ albums: response.data }));
     }
 
   renderAlbums() {
-    return this.state.albums.map(album => <Text key={album.title}>{album.title}</Text>);
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title} album={album} />
+    );
   }
 
   render() {
     return (
-      <View>
+      <ScrollView>
         {this.renderAlbums()}
-      </View>
+      </ScrollView>
     );
   }
 }
