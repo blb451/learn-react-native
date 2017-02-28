d0fc644d4fcda5306b2c66a35d981cfd043537bc
Benjamin Brenner <ben.l.brenner@gmail.com>
2017-02-28 13:35:35 -0800

    7.37

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>Album List</Text>
-    </View>
-  );
-};
+class AlbumList extends Component {
+  state = { albums: [] };
+
+  componentWillMount() {
+      axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
+    }
+
+  renderAlbums() {
+    return this.state.albums.map(album => <Text key={album.title}>{album.title}</Text>);
+  }
+
+  render() {
+    return (
+      <View>
+        {this.renderAlbums()}
+      </View>
+    );
+  }
+}
 
 export default AlbumList;
