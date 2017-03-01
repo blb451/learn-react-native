d6700367d03626ce63d56b6d06df37408e130cde
Benjamin Brenner <ben.l.brenner@gmail.com>
2017-02-28 16:11:20 -0800

    9.58

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Header } from './components/common';
+import firebase from 'firebase';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyA_gnALYcSFNSgMuKrg9JfRzOj8Yua2SEA',
+      authDomain: 'auth-32f60.firebaseapp.com',
+      databaseURL: 'https://auth-32f60.firebaseio.com',
+      storageBucket: 'auth-32f60.appspot.com',
+      messagingSenderId: '1016888651434'
+    });
+  }
+
   render() {
     return (
       <View>
