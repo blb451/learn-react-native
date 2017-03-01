6615fc0ea0558bb0c047f2ddf5cc3c994c84bfe9
Benjamin Brenner <ben.l.brenner@gmail.com>
2017-02-28 16:47:19 -0800

    9.67

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
+  state = { email: '', password: '' };
+
   render() {
     return (
       <Card>
-        <CardSection></CardSection>
-        <CardSection></CardSection>
+        <CardSection>
+          <Input
+            placeholder="user@email.com"
+            label="Email"
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
+          />
+        </CardSection>
+        <CardSection>
+          <Input
+            secureTextEntry
+            placeholder="mypassword123"
+            label="Password"
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+          />
+        </CardSection>
         <CardSection>
           <Button>
             Log In
