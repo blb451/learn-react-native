fadc49fa5e1498f93c77d0826346ce44df6be5ec
Benjamin Brenner <ben.l.brenner@gmail.com>
2017-03-01 12:44:33 -0800

    10.74

--- auth/src/App.js
+++ auth/src/App.js
@@ -6,6 +6,8 @@ import { Header } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: false };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyA_gnALYcSFNSgMuKrg9JfRzOj8Yua2SEA',
@@ -13,6 +15,14 @@ class App extends Component {
       databaseURL: 'https://auth-32f60.firebaseio.com',
       storageBucket: 'auth-32f60.appspot.com',
       messagingSenderId: '1016888651434'
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
     });
   }
 
