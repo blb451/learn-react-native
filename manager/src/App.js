8244852b9c877c12a42ac6e573bca0ae174c8961
Benjamin Brenner <ben.l.brenner@gmail.com>
2017-03-02 13:19:42 -0800

    15.107

--- manager/src/App.js
+++ manager/src/App.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 
 import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyCBv2K7OMV5-txpLtoxmQsS_HIyVuNUITg',
+      authDomain: 'manager-9c9df.firebaseapp.com',
+      databaseURL: 'https://manager-9c9df.firebaseio.com',
+      storageBucket: 'manager-9c9df.appspot.com',
+      messagingSenderId: '961536398854'
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
