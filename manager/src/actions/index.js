749ccac717a8b8279d491d91529f2f7a1f82add9
Benjamin Brenner <ben.l.brenner@gmail.com>
2017-03-02 15:23:23 -0800

    16.116

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -1,4 +1,9 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from './types';
+import firebase from 'firebase';
+
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED,
+} from './types';
 
 export const emailChanged = (text) => {
   return {
@@ -12,4 +17,9 @@ export const passwordChanged = (text) => {
     type: PASSWORD_CHANGED,
     payload: text
   };
+};
+
+export const loginUser = ({ email, password }) => {
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(user => console.log(user));
 };
