8b93a9a22d8a8d04185825490988fa347ca0e1a5
Benjamin Brenner <ben.l.brenner@gmail.com>
2017-03-03 14:05:12 -0800

    18.143

--- manager/src/actions/EmployeeActions.js
+++ manager/src/actions/EmployeeActions.js
@@ -1,8 +1,20 @@
-import { EMPLOYEE_UPDATE } from './types';
+import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
+import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
   return {
     type: EMPLOYEE_UPDATE,
     payload: { prop, value }
+  };
+};
+
+export const employeeCreate = ({ name, phone, shift }) => {
+  const { currentUser } = firebase.auth();
+
+  return () => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+    .push({ name, phone, shift })
+      .then(() => Actions.employeeList());
   };
 };
