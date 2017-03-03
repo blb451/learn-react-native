98eec389ca9559e5eb522dfe338d8c86f00fe9dc
Benjamin Brenner <ben.l.brenner@gmail.com>
2017-03-03 12:35:02 -0800

    17.134

--- manager/src/components/EmployeeCreate.js
+++ manager/src/components/EmployeeCreate.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Card, CardSection, Input, Button } from './common';
+// import { View, Text } from 'react-native';
 // import { connect } from 'react-redux';
+// import * as actions from './../actions';
 
 class EmployeeCreate extends Component {
   render() {
     return (
-      <View>
-        <Text>Employee Form</Text>
-      </View>
+      <Card>
+        <CardSection>
+          <Input
+            label="Name"
+            placeholder="Jane Doe"
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            label="Phone"
+            placeholder="555-555-5555"
+          />
+        </CardSection>
+
+        <CardSection></CardSection>
+
+        <CardSection>
+          <Button>
+            Create
+          </Button>
+        </CardSection>
+      </Card>
     );
   }
 }
